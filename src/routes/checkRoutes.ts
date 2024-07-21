b01f47649cf723d56fd96b115a54ff28fcb48d4b
Namo <pongsathon149@gmail.com>
2024-07-21 22:18:10 +0700

    feat: Add checkRoutes endpoint to retrieve server status

---
+++ src/routes/checkRoutes.ts
@@ -0,0 +1,18 @@
+import { Request, Response, Router } from "express";
+import mongoose from "mongoose";
+
+const router = Router();
+
+router.get("/", async (req: Request, res: Response) => {
+  const result = await mongoose.connection.db.admin().ping();
+  const time = new Date();
+  res.json({
+    result: "ok",
+    time,
+    main_url: process.env.BASE_URL,
+    port: process.env.PORT,
+    data: result,
+  });
+});
+
+export default router;
